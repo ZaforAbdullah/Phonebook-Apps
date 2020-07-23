e736f2bbc62b0db7165c018d24dff40e6ff5078a
ZaforAbullah <33929609+ZaforAbullah@users.noreply.github.com>
2020-07-23 23:09:09 +0800

    Initial Commit

---
+++ resources/assets/js/components/About.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">Intro</h1>
+      <h2 class="subtitle">
+        Laravel VueJS Phonebook Apps
+      </h2>
+    </div>
+  </section>
+</template>
---
+++ resources/assets/js/components/Myfooter.vue
@@ -0,0 +1,17 @@
+<template>
+	<footer class="footer">
+  <div class="container">
+    <div class="content has-text-centered">
+      <p>
+        <strong>Laravel VueJS Phonebook Apps</strong> by <a href="https://www.linkedin.com/in/zaforabdullah/">Zafor</a>. Access the source code
+        <a href="https://github.com/ZaforAbdullah/Phonebook-Apps">Laravel VueJS Phonebook Apps</a>. 
+      </p>
+      <p>
+        <a class="icon" href="https://github.com/ZaforAbdullah">
+          <i class="fa fa-github"></i>
+        </a>
+      </p>
+    </div>
+  </div>
+</footer>
+</template>
